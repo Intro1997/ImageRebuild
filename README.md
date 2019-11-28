7823128e7f3e666be3870cdb41e0e1a0c65be8cf
Outro <765856121@qq.com>
2019-11-28 21:35:21 +0800

    The Nearest Interpolation

---
+++ README.md
@@ -0,0 +1,8 @@
+### 描述
+&emsp;&emsp;毕业设计课题选了基于深度学习的图像重建，想在这里把大部分图形重建的算法记录在这里。学习路线在[这里](https://blog.csdn.net/gwplovekimi/article/details/83041627)。由于我目前定下来的方向是图形学，所以我尽可能做完上面文章中的每个方法。选这个毕业设计也是想着为之后要学习的光线追踪去噪提供一种（或多种）解决办法。未来可能会更新用 OpenGL(C++) 实现的代码。
+### 编程语言
+&emsp;&emsp;Python
+### 目前使用的库
+&emsp;&emsp;1. numpy
+&emsp;&emsp;2. matplotlib
+&emsp;&emsp;3. PIL（Pillow）
---
+++ The_nearest_interpolation/The_nearest_interpolation.py
@@ -0,0 +1,39 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+
+def Round(num):
+    res = round(num)
+    if(res > num):
+        res -= 1
+    return res
+
+
+
+def NN_interpolation(image, scale):
+    image = np.array(image)
+    new_width = Round(image.shape[0] * scale);
+    new_height = Round(image.shape[1] * scale);
+    new_shape = image.shape[2];
+
+    scale = image.shape[0] / new_width;
+    newImage = np.zeros((new_width, new_height, new_shape), dtype=np.uint8)
+    for i in range(newImage.shape[0]):
+        for j in range(newImage.shape[1]):
+            new_i = Round(i * scale)
+            new_j = Round(j * scale)
+            newImage[i, j] = image[new_i, new_j]
+    return newImage
+
+image = Image.open('img/img3.png')
+plt.imshow(image);
+plt.show()
+
+image = Image.fromarray(NN_interpolation(image, 5))
+
+plt.imshow(image);
+plt.show()
+image.save('res/res.png')
+
+
+
